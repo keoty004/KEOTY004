78924fcd09a6322c52e96f27facafd40ee1b9d4b
Thomas.Keo <keoty004@mymail.unisa.edu.au>
2023-05-24 16:45:45 +0930

    Added provided code and initialised the workshop

--- Assignment 2 - Implementation.py
+++ Assignment 2 - Implementation.py
@@ -2,6 +2,172 @@
 File: Assingment 2 - Implementation.py
 Description: 
 Author: Thomas Keo
-StudentID:
-EmailID: KEOTY004   
+StudentID: 110349388
+EmailID: KEOTY004 
+This is my own work as defined by the University's Academic Misconduct Policy.  
 """
+
+from abc import ABC
+
+class Material(ABC):
+    def __init__(self, strength):
+        self.strength = strength
+
+class Wood(Material):
+    def __init__(self, strength):
+        super().__init__(strength)
+
+class Metal(Material):
+    def __init__(self, strength, purity):
+        super().__init__(strength)
+        self.purity = purity
+
+class Gemstone(Material):
+    def __init__(self, strength, magicPower):
+        super().__init__(strength)
+        self.magicPower = magicPower
+
+class Maple(Wood):
+    def __init__(self, strength=5):
+        super().__init__(strength)
+
+class Ash(Wood):
+    def __init__(self, strength=3):
+        super().__init__(strength)
+
+class Oak(Wood):
+    def __init__(self, strength=4):
+        super().__init__(strength)
+
+class Bronze(Metal):
+    def __init__(self, strength=3, purity=1.3):
+        super().__init__(strength, purity)
+
+class Iron(Metal):
+    def __init__(self, strength=6, purity=1.1):
+        super().__init__(strength, purity)
+
+class Steel(Metal):
+    def __init__(self, strength=10, purity=1.8):
+        super().__init__(strength, purity)
+
+class Ruby(Gemstone):
+    def __init__(self, strength=1, magicPower=1.8):
+        super().__init__(strength, magicPower)
+
+class Sapphire(Gemstone):
+    def __init__(self, strength=1.2, magicPower=1.6):
+        super().__init__(strength, magicPower)
+
+class Emerald(Gemstone):
+    def __init__(self, strength=1.6, magicPower=1.1):
+        super().__init__(strength, magicPower)
+
+class Diamond(Gemstone):
+    def __init__(self, strength=2.1, magicPower=2.2):
+        super().__init__(strength, magicPower)
+
+class Amethyst(Gemstone):
+    def __init__(self, strength=1.8, magicPower=3.2):
+        super().__init__(strength, magicPower)
+
+class Onyx(Gemstone):
+    def __init__(self, strength=0.1, magicPower=4.6):
+        super().__init__(strength, magicPower)
+
+
+class Workshop:
+    def __init__(self, forge, enchanter):
+        self.enchanter = enchanter
+        self.forge = forge
+        self.weapon = []
+        self.enchantment = []
+        self.material = {}
+
+
+
+
+
+
+
+
+
+
+
+# Create a workshop, forge, enchanter.
+workshop = Workshop(Forge(), Enchanter())
+
+# Create a set of materials and lists for testing.
+materials = [Maple(), Oak(), Ash(), Bronze(), Iron(), Steel(),
+             Ruby(), Sapphire(), Emerald(), Diamond(), Amethyst(), Onyx()]
+
+weaponBlueprints = {
+    "Sword": [Steel(), Maple()],
+    "Shield": [Bronze(), Oak()],
+    "Axe": [Iron(), Ash()],
+    "Scythe": [Steel(), Ash()],
+    "Bow": [Oak(), Maple()],
+    "Wand": [Ash(), Oak()],
+    "Staff": [Bronze(), Maple()],
+    "Dagger": [Bronze(), Bronze()]}
+
+enchantmentBlueprints = {
+    "Holy": [Diamond(), Diamond()],
+    "Lava": [Ruby(), Onyx()],
+    "Pyro": [Ruby(), Diamond()],
+    "Darkness": [Onyx(), Amethyst()],
+    "Cursed": [Onyx(), Onyx()],
+    "Hydro": [Sapphire(), Emerald()],
+    "Venomous": [Emerald(), Amethyst()],
+    "Earthly": [Emerald(), Emerald()]}
+
+enchantedWeapons = ["Holy Greatsword", "Molten Defender", "Berserker Axe", "Soul Eater",
+    "Twisted Bow", "Wand of the Deep", "Venemous Battlestaff"]
+
+# Adds a number of materials to use for crafting.
+for material in materials:
+    if isinstance(material, Wood):
+        workshop.addMaterial(material.__class__.__name__, 20)
+    elif isinstance(material, Metal):
+        workshop.addMaterial(material.__class__.__name__, 10)
+    else:
+        workshop.addMaterial(material.__class__.__name__, 5)
+
+print("--------------------------------Material Store--------------------------------")
+print(workshop.displayMaterials())
+
+# Crafts the following: Sword, Shield, Axe, Scythe, Bow, Wand and Staff weapons.
+for weapon, materials in weaponBlueprints.items():
+    craftedWeapon = workshop.forge.craft(
+        weapon, materials[0], materials[1], workshop.materials)
+    workshop.addWeapon(craftedWeapon)
+
+# Disassemble the extra weapon.
+workshop.removeWeapon(workshop.forge.disassemble(
+    workshop.weapons[7], workshop.materials))
+
+print("------------------------------------Armoury-----------------------------------")
+print(workshop.displayWeapons())
+
+# Crafts the following: Holy, Lava, Pyro, Darkness, Cursed, Hydro and Venomousenchantments.
+for enchantment, materials in enchantmentBlueprints.items():
+    craftedEnchantment = workshop.enchanter.craft(
+    enchantment, materials[0], materials[1], workshop.materials)
+    workshop.addEnchantment(craftedEnchantment)
+
+# Disassemble the extra enchantment.
+workshop.removeEnchantment(workshop.enchanter.disassemble(
+    workshop.enchantments[7], workshop.materials))
+
+print("------------------------------------Enchantments------------------------------------")
+print(workshop.displayEnchantments())
+
+print("-----------------------------------Material Store-----------------------------------")
+print(workshop.displayMaterials())
+
+# Enchant the following weapons: Sword, Shield, Axe, Scythe, Bow, Wand and Staff.
+for i in range(len(enchantedWeapons)):
+    workshop.enchanter.enchant(
+        workshop.weapons[i], enchantedWeapons[i], workshop.enchantments[i])
+print("-----------------------------------Enchanted Armoury----------------------------------")
+print(workshop.displayWeapons())
